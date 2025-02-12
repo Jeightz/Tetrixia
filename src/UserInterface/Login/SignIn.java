a878e580769b7755dcfabbbbacde222a3827041e
Jeightz <t4nyou2@gmail.com>
2025-02-12 16:04:59 +0800

    erase the raining tetro class

--- src/UserInterface/Login/SignIn.java
+++ src/UserInterface/Login/SignIn.java
@@ -109,7 +109,6 @@ public class SignIn extends javax.swing.JFrame {
         cbgender = new javax.swing.JComboBox<>();
         jLabel3 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
-        myRainingTetroAnimation1 = new UserInterface.CustomComponents.MyRainingTetroAnimation(this);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Signup");
@@ -464,31 +463,15 @@ public class SignIn extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(0, 153, 0));
 
-        javax.swing.GroupLayout myRainingTetroAnimation1Layout = new javax.swing.GroupLayout(myRainingTetroAnimation1);
-        myRainingTetroAnimation1.setLayout(myRainingTetroAnimation1Layout);
-        myRainingTetroAnimation1Layout.setHorizontalGroup(
-            myRainingTetroAnimation1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 334, Short.MAX_VALUE)
-        );
-        myRainingTetroAnimation1Layout.setVerticalGroup(
-            myRainingTetroAnimation1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 503, Short.MAX_VALUE)
-        );
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(myRainingTetroAnimation1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(272, 272, 272))
+            .addGap(0, 350, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(myRainingTetroAnimation1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 27, Short.MAX_VALUE))
+            .addGap(0, 530, Short.MAX_VALUE)
         );
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 350, 530));
@@ -738,7 +721,6 @@ public class SignIn extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lblPictureHolder;
     private UserInterface.CustomComponents.MyButton myButton1;
-    private UserInterface.CustomComponents.MyRainingTetroAnimation myRainingTetroAnimation1;
     private UserInterface.CustomComponents.MyPanel signIn;
     private UserInterface.CustomComponents.MyPanel signUp;
     private javax.swing.JTabbedPane tabLogInOut;
